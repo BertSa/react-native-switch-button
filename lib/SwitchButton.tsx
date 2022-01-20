2796f93f139a32b00f55dea39f75be6a2a61d96d
Samuel Bertrand <bertsa.pro@gmail.com>
2022-01-19 19:58:12 -0500

    Revert "isActive props instead of state"
    This reverts commit 2b986966377d90f38f7826c2e5628e3e668f91c6.

--- lib/SwitchButton.tsx
+++ lib/SwitchButton.tsx
@@ -59,6 +59,9 @@ export default class SwitchButton extends React.Component<
   constructor(props: ISwitchButtonProps) {
     super(props);
     this.interpolatedColor = new Animated.Value(ORIGINAL_VALUE);
+    this.state = {
+      isActive: false,
+    };
   }
 
   componentDidMount() {
@@ -73,7 +76,9 @@ export default class SwitchButton extends React.Component<
 
   handleActiveState = () => {
     if (this.props.isActive) {
-        this.props.isActive ? this.showFocusColor() : this.showOriginColor();
+      this.setState({ isActive: !this.state.isActive }, () => {
+        this.state.isActive ? this.showFocusColor() : this.showOriginColor();
+      });
     }
   };
 
@@ -94,9 +99,9 @@ export default class SwitchButton extends React.Component<
   };
 
   handlePress = () => {
-    this.setState({ isActive: !this.props.isActive }, () => {
-      this.props.isActive ? this.showFocusColor() : this.showOriginColor();
-      this.props.onPress && this.props.onPress(this.props.isActive as boolean);
+    this.setState({ isActive: !this.state.isActive }, () => {
+      this.state.isActive ? this.showFocusColor() : this.showOriginColor();
+      this.props.onPress && this.props.onPress(this.state.isActive);
     });
   };
 
@@ -114,10 +119,10 @@ export default class SwitchButton extends React.Component<
     const mainColor = this.props.mainColor || MAIN_COLOR;
     const originalColor = this.props.originalColor || ORIGINAL_COLOR;
     const tintColor = this.props.tintColor || TINT_COLOR;
-    const _animatedBGColorOutputRange = this.props.isActive
+    const _animatedBGColorOutputRange = this.state.isActive
       ? [mainColor, originalColor]
       : [originalColor, mainColor];
-    const _animatedTintColorOutputRange = this.props.isActive
+    const _animatedTintColorOutputRange = this.state.isActive
       ? [originalColor, tintColor]
       : [tintColor, originalColor];
     let animatedBackgroundColor = this.interpolatedColor.interpolate({
@@ -134,7 +139,7 @@ export default class SwitchButton extends React.Component<
         onPress={this.handlePress}
       >
         <Animated.Image
-          source={this.props.isActive ? activeImageSource : inactiveImageSource}
+          source={this.state.isActive ? activeImageSource : inactiveImageSource}
           style={[_imageStyle(animatedTintColor), imageStyle]}
         />
       </AnimatedRNBounceable>
