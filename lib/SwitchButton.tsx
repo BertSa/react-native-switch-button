0dcd9d1ba1d8f9bb13c4b46cbea54e083c71d087
Samuel Bertrand <bertsa.pro@gmail.com>
2022-01-19 21:17:21 -0500

    Change class to function

--- lib/SwitchButton.tsx
+++ lib/SwitchButton.tsx
@@ -1,19 +1,11 @@
 import * as React from "react";
-import {
-  View,
-  Text,
-  Animated,
-  ViewStyle,
-  StyleProp,
-  TextStyle,
-  ImageStyle,
-  ImageSourcePropType,
-} from "react-native";
+import {useEffect} from "react";
+import {Animated, Text, View,} from "react-native";
 import RNBounceable from "@freakycoder/react-native-bounceable";
 /**
  * ? Local Imports
  */
-import styles, { _containerStyle, _imageStyle } from "./SwitchButton.style";
+import styles, {_containerStyle, _imageStyle} from "./SwitchButton.style";
 
 const AnimatedRNBounceable = Animated.createAnimatedComponent(RNBounceable);
 
@@ -23,146 +15,105 @@ const TINT_COLOR = "#f1bb7b";
 const ORIGINAL_VALUE = 0;
 const ANIMATED_VALUE = 1;
 
-type CustomStyleProp = StyleProp<ViewStyle> | Array<StyleProp<ViewStyle>>;
-type CustomTextStyleProp = StyleProp<TextStyle> | Array<StyleProp<TextStyle>>;
-type CustomImageStyleProp =
-  | StyleProp<ImageStyle>
-  | Array<StyleProp<ImageStyle>>;
 
-interface ISwitchButtonProps {
-  style?: CustomStyleProp;
-  textStyle?: CustomTextStyleProp;
-  imageStyle?: CustomImageStyleProp;
-  textContainerStyle?: CustomTextStyleProp;
-  activeImageSource: ImageSourcePropType;
-  inactiveImageSource: ImageSourcePropType;
-  text?: string;
-  mainColor?: string;
-  tintColor?: string;
-  sameTextColor?: boolean;
-  isActive: boolean;
-  handleChange: React.Dispatch<React.SetStateAction<boolean>>;
-  disableText?: boolean;
-  originalColor?: string;
-  onPress: (isActive: boolean) => void;
-}
+export default function SwitchButton(props) {
+    let interpolatedColor: Animated.Value;
+    useEffect(() => {
+        interpolatedColor = new Animated.Value(ORIGINAL_VALUE);
+    }, [])
 
-interface IState {
-  isActive: boolean;
-}
+    useEffect(() => {
+        handleActiveState();
+    }, [props.isActive])
 
-export default class SwitchButton extends React.Component<
-  ISwitchButtonProps,
-  IState
-> {
-  interpolatedColor: Animated.Value;
+    const handleActiveState = () => {
+        props.isActive ? showFocusColor() : showOriginColor();
+    };
 
-  constructor(props: ISwitchButtonProps) {
-    super(props);
-    this.interpolatedColor = new Animated.Value(ORIGINAL_VALUE);
-  }
+    const showOriginColor = () => {
+        Animated.timing(interpolatedColor, {
+            duration: 350,
+            toValue: ORIGINAL_VALUE,
+            useNativeDriver: false,
+        }).start();
+    };
 
-  componentDidMount() {
-    this.handleActiveState();
-  }
+    const showFocusColor = () => {
+        Animated.timing(interpolatedColor, {
+            duration: 450,
+            toValue: ANIMATED_VALUE,
+            useNativeDriver: false,
+        }).start();
+    };
 
+    const handlePress = () => {
+        props.handleChange(!props.isActive);
+        props.isActive ? showFocusColor() : showOriginColor();
+        props.onPress && props.onPress(props.isActive);
+    };
 
-  handleActiveState = () => {
-    this.props.isActive ? this.showFocusColor() : this.showOriginColor();
-  };
+    /* -------------------------------------------------------------------------- */
+    /*                               Render Methods                               */
+    /* -------------------------------------------------------------------------- */
 
-  showOriginColor = () => {
-    Animated.timing(this.interpolatedColor, {
-      duration: 350,
-      toValue: ORIGINAL_VALUE,
-      useNativeDriver: false,
-    }).start();
-  };
+    const renderBounceableButton = () => {
+        const {
+            style,
+            imageStyle,
+            activeImageSource,
+            inactiveImageSource,
+        } = props;
+        const mainColor = props.mainColor || MAIN_COLOR;
+        const originalColor = props.originalColor || ORIGINAL_COLOR;
+        const tintColor = props.tintColor || TINT_COLOR;
+        let animatedBackgroundColor = interpolatedColor.interpolate({
+            inputRange: [ORIGINAL_VALUE, ANIMATED_VALUE],
+            outputRange: [originalColor, mainColor],
+        });
+        let animatedTintColor = interpolatedColor.interpolate({
+            inputRange: [ORIGINAL_VALUE, ANIMATED_VALUE],
+            outputRange: [tintColor, originalColor],
+        });
+        return (
+            <AnimatedRNBounceable
+                style={[_containerStyle(animatedBackgroundColor), style]}
+                onPress={handlePress}
+            >
+                <Animated.Image
+                    source={props.isActive ? activeImageSource : inactiveImageSource}
+                    style={[_imageStyle(animatedTintColor), imageStyle]}
+                />
+            </AnimatedRNBounceable>
+        );
+    };
 
-  showFocusColor = () => {
-    Animated.timing(this.interpolatedColor, {
-      duration: 450,
-      toValue: ANIMATED_VALUE,
-      useNativeDriver: false,
-    }).start();
-  };
+    const renderText = () => {
+        const {text, textStyle, textContainerStyle} = props;
+        if (props.disableText) {
+            return null;
+        }
 
-  handlePress = () => {
-      this.props.handleChange(!this.props.isActive);
-      this.props.isActive ? this.showFocusColor() : this.showOriginColor();
-      this.props.onPress && this.props.onPress(this.props.isActive);
-  };
+        if (props.sameTextColor) {
+            let mainColor = props.mainColor || MAIN_COLOR;
+            let tintColor = props.tintColor || TINT_COLOR;
+            const animatedTextColor = interpolatedColor.interpolate({
+                inputRange: [ORIGINAL_VALUE, ANIMATED_VALUE],
+                outputRange: [tintColor, mainColor]
+            });
+            return <Animated.View style={[styles.textContainerStyle, textContainerStyle]}>
+                <Animated.Text style={{...(textStyle as object), color: animatedTextColor}}>{text}</Animated.Text>
+            </Animated.View>;
+        }
 
-  /* -------------------------------------------------------------------------- */
-  /*                               Render Methods                               */
-  /* -------------------------------------------------------------------------- */
+        return <View style={[styles.textContainerStyle, textContainerStyle]}>
+            <Text style={textStyle}>{text}</Text>
+        </View>;
+    };
 
-  renderBounceableButton = () => {
-    const {
-      style,
-      imageStyle,
-      activeImageSource,
-      inactiveImageSource,
-    } = this.props;
-    const mainColor = this.props.mainColor || MAIN_COLOR;
-    const originalColor = this.props.originalColor || ORIGINAL_COLOR;
-    const tintColor = this.props.tintColor || TINT_COLOR;
-    const _animatedBGColorOutputRange = this.props.isActive
-      ? [mainColor, originalColor]
-      : [originalColor, mainColor];
-    const _animatedTintColorOutputRange = this.props.isActive
-      ? [originalColor, tintColor]
-      : [tintColor, originalColor];
-    let animatedBackgroundColor = this.interpolatedColor.interpolate({
-      inputRange: [ORIGINAL_VALUE, ANIMATED_VALUE],
-      outputRange: [originalColor, mainColor],
-    });
-    let animatedTintColor = this.interpolatedColor.interpolate({
-      inputRange: [ORIGINAL_VALUE, ANIMATED_VALUE],
-      outputRange: [tintColor, originalColor],
-    });
     return (
-      <AnimatedRNBounceable
-        style={[_containerStyle(animatedBackgroundColor), style]}
-        onPress={this.handlePress}
-      >
-        <Animated.Image
-          source={this.props.isActive ? activeImageSource : inactiveImageSource}
-          style={[_imageStyle(animatedTintColor), imageStyle]}
-        />
-      </AnimatedRNBounceable>
+        <View style={styles.container}>
+            {renderBounceableButton()}
+            {renderText()}
+        </View>
     );
-  };
-
-  renderText = () => {
-    const { text, textStyle, textContainerStyle } = this.props;
-    if (this.props.disableText) {
-      return null;
-    }
-
-    if (this.props.sameTextColor) {
-      let mainColor = this.props.mainColor || MAIN_COLOR;
-      let tintColor = this.props.tintColor || TINT_COLOR;
-      const animatedTextColor = this.interpolatedColor.interpolate({
-        inputRange: [ORIGINAL_VALUE, ANIMATED_VALUE],
-        outputRange: [tintColor, mainColor]
-      });
-      return <Animated.View style={[styles.textContainerStyle, textContainerStyle]}>
-        <Animated.Text style={{ ...(textStyle as object), color: animatedTextColor }}>{text}</Animated.Text>
-      </Animated.View>;
-    }
-
-    return <View style={[styles.textContainerStyle, textContainerStyle]}>
-      <Text style={textStyle}>{text}</Text>
-    </View>;
-  };
-
-  render() {
-    return (
-      <View style={styles.container}>
-        {this.renderBounceableButton()}
-        {this.renderText()}
-      </View>
-    );
-  }
 }
